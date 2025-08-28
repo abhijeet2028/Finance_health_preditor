541bf9e9a5aac1d415396e651048af5d02e7527b
Abhijeet Warale <113260198+abhijeet2028@users.noreply.github.com>
2025-08-28 09:06:34 +0530

    Add files via upload

---
+++ Frontend/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    open: true, // Automatically opens browser on npm run dev
+  },
+})
